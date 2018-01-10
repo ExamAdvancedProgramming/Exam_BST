df03f3f74dd3ffb61560076e5941878f7d666f26
Maria Strazzullo <mstrazzu@sissa.it>
2018-01-10 20:01:23 +0100

    Iterator and print working

--- Skeleton.cc
+++ Skeleton.cc
@@ -13,25 +13,24 @@ class BST {
   key_type key;
   std::unique_ptr<Node> left;
   std::unique_ptr<Node> right;
-  std::unique_ptr<Node> parent;
-  Node(const key_type  k, const value_type  v, Node * p = nullptr)  : value{v}, key{k}, parent{p}, left{nullptr}, right{nullptr} { }
-  
+  Node* parent;
+  Node(const key_type  k, const value_type  v, Node* p = nullptr)  : value{v}, key{k}, parent{p}, left{nullptr}, right{nullptr} {}
  
   };
   
-  std::unique_ptr<Node> root;
   
-  
+    std::unique_ptr<Node> root;
   
   public:
   BST() : root{nullptr} {};
+   ~ BST (){}
   void insert (const key_type  k, const value_type  v);
   void print() const;
   void clear() const;
   void balance();
   void erase();
   
-   Node* operator=(const Node* m) {
+   /*std::unique_ptr<Node> operator=(const Node* m) {
     if (m != this) {
         this -> left = m -> left;
         this -> right = m -> right;
@@ -43,15 +42,20 @@ class BST {
     
     return *this;
     }
+    */
   
   class Iterator;
   
-  Iterator begin() {Node* first_key = root.get();
+ 
+  
+  
+  Iterator begin() 
+  {Node* first_key = root.get();
   while(first_key -> left.get()){
   first_key = first_key -> left.get();
   };
   std::cout <<"sono qui "<< first_key -> key <<std::endl;
-  return Iterator{first_key};
+  return Iterator{first_key, root.get()};
   }
   Iterator end() { 
   Node* last_key = root.get();
@@ -59,7 +63,7 @@ class BST {
   last_key = last_key -> right.get();
   };
   std::cout <<"sono qui "<< last_key -> key <<std::endl;
-  return Iterator{last_key};
+  return Iterator{last_key, root.get()};
    }
 
   class ConstIterator;
@@ -69,7 +73,7 @@ class BST {
   first_key = first_key -> left.get();
   };
   std::cout <<"sono qui "<< first_key -> key <<std::endl;
-  return ConstIterator{first_key}; }
+  return ConstIterator{first_key, root.get()}; }
   
   ConstIterator cend() const {
   Node* last_key = root.get();
@@ -77,7 +81,7 @@ class BST {
   last_key = last_key -> right.get();
   };
   std::cout <<"sono qui "<< last_key -> key <<std::endl;
-  return ConstIterator{last_key};
+  return ConstIterator{last_key, root.get()};
    }
   
   Iterator find(key_type& k){
@@ -95,33 +99,38 @@ class BST {
 
 template < typename key_type, typename value_type>
 
-class BST<key_type, value_type>::Iterator {
+class BST<key_type, value_type>::Iterator  {
   using Node = BST<key_type, value_type>::Node;
   Node* current;
+  Node* _root;
 
  public:
-  Iterator(Node* n) : current{n} {}
-  value_type& operator*() const { return current->value; }
+  Iterator(Node* n, Node* root) : current{n}, _root{root} {};
+  value_type& operator*() const { return current->key; } /// VALUEEEE
 
   // ++it
   Iterator& operator++() {
-    if (current == root.get())          //RADICE
-    { current = current -> right.get();
-    while(current -> left.get()){
-    current = current.left();
+    Node* last = _root;
+    while(last -> right){
+    last = last -> right.get();
     }
+    if (current == last){
+    //std::cout << "ci passo" << std::endl;
+    return *this;
     }
-    if(current -> key < root -> key){
-    if (current -> right.get() = nullptr){
-    current = current -> parent.get();
+    else if(current -> right.get() == nullptr && current -> parent -> right.get() == current){
+    current = current -> parent -> parent;
     }
-    else{
+    else if(current -> right.get() == nullptr && current -> parent -> left.get() == current){ 
+    current = current -> parent;
+    }
+    else if(current -> right.get() != nullptr){
     current = current -> right.get();
     while(current -> left.get()){
-    current = current.left();
+    current = current -> left.get();
     }
     }
-    }
+   
     //current = current->right.get();
     return *this;
   }
@@ -143,17 +152,18 @@ template < typename key_type, typename value_type>
 
 //template <typename types>
 class BST<key_type,value_type> ::ConstIterator : public BST<key_type,value_type> ::Iterator {
-  using parent = BST<key_type,value_type> ::Iterator;
+  using parents = BST<key_type,value_type> ::Iterator;
 
  public:
-  using parent::Iterator;
+  using parents::Iterator;
 };
 
 /*
 template < typename key_type, typename value_type>
  Iterator<key_type,value_type>::begin(){ 
   
-  Node* first_key = root.get();
+  Node* first_key = root.get();4 4
+
   while(first_key -> left.get()){
   first_key = first_key -> left.get();
   };
@@ -168,14 +178,13 @@ void BST<key_type,value_type>::insert(const key_type k, const value_type v){
     if (root == nullptr){
     std::cout << "sono nullo" << std::endl;
     root.reset(new Node{k,v});
-    root -> parent.reset(nullptr);
     return; 
     }
     else {
     std::cout << "non sono nullo" << std::endl;
     Node* next = root.get(); 
     while(next){
-    //std::cout << next -> key << std::endl;
+    std::cout << next -> key << std::endl;
     //temp -> parent = std::move(temp);
     if(k < next -> key){
     std::cout << "left!" << std::endl;
@@ -203,25 +212,30 @@ void BST<key_type,value_type>::insert(const key_type k, const value_type v){
     std::cout << "fuori dal while" << std::endl; 
     if (k < temp -> key){
     std::cout << "NUOVO NODO SINISTRA" << std::endl;
-    temp -> left.reset(new Node{k,v});
-    temp -> left -> parent.reset(new Node{temp -> key, temp -> value});
-    std::cout << temp -> left -> parent -> key << " " << temp -> key  << std::endl;
-    if(temp -> parent != nullptr){
-    std::cout << "SONO TUO PADRE!" << std::endl;}
+    temp -> left.reset(new Node{k,v, temp});
+   std::cout << temp -> left -> parent -> key << " " << temp -> key << std::endl;
     }
     
     else{
     std::cout << "NUOVO NODO DESTRA" << std::endl;
-    temp -> right.reset(new Node{k,v});
-    temp -> right -> parent.reset(new Node{temp -> key, temp -> value});
-    std::cout << temp -> right -> parent -> key << " " << temp -> key  << std::endl;
-     if(temp -> parent != nullptr){
-    std::cout << "SONO TUO PADRE!" << std::endl;}
-    }
+    temp -> right.reset(new Node{k,v, temp});
+    std::cout << temp -> right -> parent -> key << " " << temp -> key << std::endl;
+    } 
+    return;
     };
     
     
-    
+
+template <typename key_type, typename value_type>
+void BST<key_type, value_type>::print() const {
+
+
+  auto it {this->cbegin()};
+  auto it_end{this->cend()};
+  for (; it != ++it_end; ++it){
+    std::cout << *it << " ";}
+    std::cout << *it_end << std::endl;
+}   
 
 /*
 template < typename key_type, typename value_type>
@@ -240,14 +254,21 @@ Node* first_key = root.get();
 
 int main() {
   BST<int, int> Tree{};
-  Tree.insert(2,2);
-  //Tree.insert(2,2);
-  Tree.insert(4,3);
-  Tree.insert(3,3);
-  Tree.insert(2,2);
-  Tree.insert(1,1);
+  Tree.insert(8,2);
   Tree.insert(5,2);
+  Tree.insert(3,3);
+  Tree.insert(7,3);
+  Tree.insert(4,2);
+  Tree.insert(14,1);
   Tree.insert(6,2);
+  Tree.insert(9,2);
+  Tree.insert(13,2);
+  Tree.insert(15,2);
+  Tree.insert(10,2);
+  Tree.insert(20,2);
+  Tree.insert(30,2);
+  Tree.insert(16,2);
+  Tree.print();
   BST<int, int>::Iterator first = Tree.begin();
   BST<int, int>::Iterator last = Tree.end();
   BST<int, int>::ConstIterator cfirst = Tree.cbegin();
